5d36d032a61a0bca5dcac22641c4a37e33ea46c0
Branzz03 <akmal.tbn@gmail.com>
2025-01-11 21:42:10 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-    home:  const HomeScreen(), // Set SplashScreen sebagai halaman awal
+    home:  const SplashScreen(), // Set SplashScreen sebagai halaman awal
     );
   }
 }
